42c31947c4a26b2cf8cec25d30243dbb7439f9b5
Alizee42 <alizee.gueye@gmail.com>
2023-09-26 12:15:12 +0200

    ajout observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { interval, Observable } from 'rxjs';
+import {filter, map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-   
-  ngOnInit() {} 
+
+  interval$!: Observable<string>;
+
+  ngOnInit() {
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0),
+      map(value => value % 2 === 0 ?
+          `Je suis ${value} et je suis pair` :
+          `Je suis ${value} et je suis impair`
+      ),
+      tap(text => this.logger(text))
+  );
+  }
+
+  logger(text: string): void {
+    console.log(`Log: ${text}`);
 }
+}
+
